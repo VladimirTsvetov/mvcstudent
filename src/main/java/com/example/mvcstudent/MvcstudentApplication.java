5bb00a4946690e553e6940d4044da69b697a5418
VladimirTsvetov <v.a.tsvetov@gmail.com>
2023-04-27 21:45:33 +0300

    Initial commit

---
+++ src/main/java/com/example/mvcstudent/MvcstudentApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvcstudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcstudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcstudentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mvcstudent/MvcstudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcstudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcstudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
